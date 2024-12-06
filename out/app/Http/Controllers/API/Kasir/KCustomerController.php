c8c1b196398de5acf17ef75b0dbd4634f012d0b3
arindyproject <ajiariadam@gmail.com>
2024-12-06 13:21:39 +0700

    dsfsdf

--- out/app/Http/Controllers/API/Kasir/KCustomerController.php
+++ out/app/Http/Controllers/API/Kasir/KCustomerController.php
@@ -5,6 +5,19 @@ namespace App\Http\Controllers\API\Kasir;
 use App\Http\Controllers\Controller; 
 use Illuminate\Http\Request; 
 use Illuminate\Support\Facades\Auth; 
+use Illuminate\Support\Facades\Validator; 
+
+use App\Http\Resources\BaseResource; 
  
 Class KCustomerController extends Controller { 
+    //construct----------------------------------------------
+    public function __construct(){
+    }
+    //end_construct------------------------------------------
+
+    //index--------------------------------------------------
+    public function index(){
+    }
+    //end_index----------------------------------------------
+
 } 
